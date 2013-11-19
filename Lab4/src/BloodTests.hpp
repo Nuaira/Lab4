c6218d11b0d5341b20d386ba551d2af752d37385
Nuaira M <n36moham@ryerson.ca>
2013-11-19 13:18:18 -0500

    Deleted previous files because only one hpp file is needed for lab. General outline of this hpp file is created

---
+++ Lab4/src/BloodTests.hpp
@@ -0,0 +1,52 @@
+/*
+ * BloodTests.hpp
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: n36moham
+ */
+
+#ifndef BLOODTESTS_HPP_
+#define BLOODTESTS_HPP_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BloodTests{	// Base Class
+
+protected:
+
+	//
+
+public:
+
+	//
+
+};
+
+class BloodCount{	// Derived Class 1
+
+public:
+
+	//
+
+};
+
+class BloodGlucose{	// Derived Class 2
+
+public:
+
+	//
+
+};
+
+class Lipoprotein{	// Derived Class 3
+
+public:
+
+	//
+
+};
+
+#endif /* BLOODTESTS_HPP_ */
