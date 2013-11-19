db6f3bce55a81c65677181568e137cce41784f09
Nuaira M <n36moham@ryerson.ca>
2013-11-19 15:14:41 -0500

    Finished prototypes for base class and BloodCount derived class

--- Lab4/src/BloodTests.hpp
+++ Lab4/src/BloodTests.hpp
@@ -17,35 +17,64 @@ class BloodTests{	// Base Class
 
 protected:
 
-	//
+	// result ranges for male and female
+	int rangemale;
+	int rangefemale;
+
+	// units can be: Cells/mcL (cells per microliter), gm/dL (grams per deciliter), femtoliter, or mg/dL (milligrams per deciliter)
+	int unit;
 
 public:
 
-	//
+	BloodTests(const int myrangemale, const int myrangefemale, const int myunit);	// Constructor
 
 };
 
 class BloodCount{	// Derived Class 1
 
+protected:
+
+	// variables for each blood count test
+	int rbcnumber;
+	int wbcnumber;
+	int pnumber;
+	int hemonumber;
+	int hemanumber;
+	int volumenumber;
+
 public:
 
-	//
+	BloodCount(const int myrangemale, const int myrangefemale, const int myunit);
+	void RedBloodCell();
+	void WhiteBloodCell();
+	void Platelets();
+	void Hemoglobin();
+	void Hematocrit();
+	void MeanCorpuscularVolume();
 
 };
 
 class BloodGlucose{	// Derived Class 2
 
-public:
+protected:
 
 	//
+
+public:
+
+	BloodGlucose(const int myrangemale, const int myrangefemale, const int myunit);
 
 };
 
 class Lipoprotein{	// Derived Class 3
 
-public:
+protected:
 
 	//
+
+public:
+
+	Lipoprotein(const int myrangemale, const int myrangefemale, const int myunit);
 
 };
 
